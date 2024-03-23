c6572706d6ea9c46c07d88893eace0fa6517db9c
killgorealpha <killgorealpha@protonmail.com>
2024-03-23 16:34:17 -0700

    modified:   crontab 	modified:   stream.sh

--- stream.sh
+++ stream.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./mediamtx &
-ffmpeg -input_format yuyv422 -f v4l2 -video_size 800x448 -framerate 30 -i /dev/video2 -c:v h264_v4l2m2m -b:v 8M -c:a copy -an -f rtsp rtsp://localhost:8554/greenhouse -rtsp_transport tcp
+ffmpeg -input_format yuyv422 -f v4l2 -video_size 864x480 -framerate 24 -i /dev/video2 -c:v h264_v4l2m2m -b:v 1000K -c:a copy -an -f rtsp rtsp://localhost:8554/greenhouse -rtsp_transport tcp
+echo "$$" > /$HOME/stream.pid
